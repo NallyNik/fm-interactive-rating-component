7bfb0f383a4d3bff1c261934ca68f81b0db7cacc
Anahita Monezi <109434062+NallyNik@users.noreply.github.com>
2023-03-29 18:40:29 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*{.html,.js}"],
+  content: [
+    "index.html",
+    "./src/**/*.js"],
   theme: {
     colors: {
       'orange': 'hsl(25, 97%, 53%)',
